15efaddf27ba2e06136b94a53c3225bf877871f9
Ernesto Resende <ernestoresende@hotmail.com>
2020-07-20 23:48:18 -0300

    27. ngIf else condition

--- 2-the-basics/src/app/servers/servers.component.ts
+++ 2-the-basics/src/app/servers/servers.component.ts
@@ -6,19 +6,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = true;
-  serverCreationStatus = 'No server was crated!'
+  serverCreationStatus = 'No server was created!'
   serverName = 'Test Server'
+  serverCreated = false // Defaults serverCreated to false at the application start
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000)
   } // Default method from TypeScript, first to be loaded.
+    // Defines a timeout the the allowNewServer event listener to be trigered.
 
   ngOnInit() {} //  lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!'
+    this.serverCreated = true // Changes the serverCreated status to true when onCreateServer method is fired trough the click event listener.
+    this.serverCreationStatus = 'Server was created! The server name is ' + this.serverName
   } // Method that is called when the "Add Server button is clicked"
 
   onUpdateServerName(event: Event) { 
